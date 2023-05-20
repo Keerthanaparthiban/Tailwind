8addd0be133a63f1698692d1becc38ef5fb597bf
Keerthanaparthiban <92458543+Keerthanaparthiban@users.noreply.github.com>
2023-05-20 15:10:53 +0530

    colors and fontFamily added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#FF6363"
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883"
+        }
       },
-      fontSize: {
-        mammoth: "8rem"
+      fontFamily: {
+        body: ['Nunito']
       }
     },
   },
